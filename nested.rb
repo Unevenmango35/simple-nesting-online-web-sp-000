e4da9ce63ae4f2455708c279537c86e8dcafbf9e
Rosie Choi <rosiechoimanso@gmail.com>
2020-09-17 03:34:16 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -15,7 +15,7 @@ def hopper
           :languages => ["C"]
         }
      }
-
+return programmer_hash[:grace_hopper]
 
 end
 
@@ -37,6 +37,8 @@ def alan_kay_is_known_for
           :languages => ["C"]
         }
      }
+    programmer_hash[:alan_kay][:known_for]
+    
 end
 
 def dennis_ritchies_language
@@ -55,6 +57,8 @@ def dennis_ritchies_language
           :languages => ["C"]
         }
      }
+    x= programmer_hash[:dennis_ritchie][:languages]
+    x.join
 end
 
 def adding_matz
@@ -80,7 +84,10 @@ def adding_matz
         }
      }
 
-    
+    programmer_hash[:yukihiro_matsumoto] = 
+    {:known_for => "Ruby",
+:languages => ["LISP", "C"]}
+programmer_hash
 end
 
 def changing_alan
